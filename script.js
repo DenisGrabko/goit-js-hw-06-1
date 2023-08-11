d1a2a604eb188d791787664190d80451b33aa9a9
Denis <grabko.denis11@gmail.com>
2023-08-12 00:16:12 +0300

    task-3-done

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict"
 
+/* //////////////////////////    1    /////////////////////////////// */ 
+/* 
 const elemList = document.querySelectorAll(".item");
 console.dir("Number of categories " + elemList.length);
 
@@ -8,6 +10,63 @@ console.dir("Number of categories " + elemList.length);
 elemList.forEach((element) => {
     console.dir("Catgory: " + element.querySelector("h2").textContent)
     console.dir("Elements: " + element.querySelectorAll("ul li").length)});
+*/
+/* //////////////////////////    1    /////////////////////////////// */ 
+
+
+
+
+
+/* //////////////////////////    2    /////////////////////////////// */ 
+/*
+const ingredients = [
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatos",
+    "Herbs",
+    "Condiments",
+  ];
+const ingredientsId = document.querySelector('#ingredients');
+
+ingredients.forEach((element) => { 
+    const liEl = document.createElement("li");
+    li.classList = "item"; 
+    li.textContent = element;
+    ingredientsId.insertAdjacentElement('beforeend', liELl);
+}
+    );
+*/
+/* //////////////////////////    2    /////////////////////////////// */ 
+
+
+
+/* //////////////////////////    3    /////////////////////////////// */
+const images = [
+    {
+      url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+      alt: "White and Black Long Fur Cat",
+    },
+    {
+      url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+      alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+    },
+    {
+      url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+      alt: "Group of Horses Running",
+    },
+  ];
+
+const ulList = document.querySelector(".gallery");
+
+images.forEach(element => {ulList.insertAdjacentHTML('beforeend' , `<li class="itemOfGallery"><img width="300px" height="200px" src=${element.url}></li>`)});
+
+
+
+
+/* //////////////////////////    3    /////////////////////////////// */ 
+
+
 
 
 
