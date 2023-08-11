10c4ae8ddd98c67ddaa143ccca060b50b7d5efff
Denis <grabko.denis11@gmail.com>
2023-08-12 00:19:36 +0300

    taska-3-allTocomment

--- script.js
+++ script.js
@@ -42,28 +42,24 @@ ingredients.forEach((element) => {
 
 
 /* //////////////////////////    3    /////////////////////////////// */
-const images = [
-    {
-      url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
-      alt: "White and Black Long Fur Cat",
-    },
-    {
-      url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
-      alt: "Orange and White Koi Fish Near Yellow Koi Fish",
-    },
-    {
-      url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
-      alt: "Group of Horses Running",
-    },
-  ];
+// const images = [
+//     {
+//       url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+//       alt: "White and Black Long Fur Cat",
+//     },
+//     {
+//       url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+//       alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+//     },
+//     {
+//       url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+//       alt: "Group of Horses Running",
+//     },
+//   ];
 
-const ulList = document.querySelector(".gallery");
+// const ulList = document.querySelector(".gallery");
 
-images.forEach(element => {ulList.insertAdjacentHTML('beforeend' , `<li class="itemOfGallery"><img width="300px" height="200px" src=${element.url}></li>`)});
-
-
-
-
+// images.forEach(element => {ulList.insertAdjacentHTML('beforeend' , `<li class="itemOfGallery"><img width="300px" height="200px" src=${element.url}></li>`)});
 /* //////////////////////////    3    /////////////////////////////// */ 
 
 
