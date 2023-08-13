1dacde5c2ae5e6486a5623d7b3fe437524764ffe
Denis <grabko.denis11@gmail.com>
2023-08-13 23:20:20 +0300

    7-10-NF

--- script.js
+++ script.js
@@ -136,6 +136,108 @@ ingredients.forEach((element) => {
 /* //////////////////////////    7    /////////////////////////////// */ 
 
 
+/* //////////////////////////    8    /////////////////////////////// */ 
+// const loginForm = document.querySelector(".login-form");
+
+// loginForm.addEventListener("submit", formFunction);
+// document.addEventListener('DOMContentLoaded', function() {
+//   const loginForm = document.querySelector('.login-form');
+
+//   loginForm.addEventListener('submit', function(event) {
+//     event.preventDefault(); 
+
+//     const email = loginForm.elements.email.value;
+//     const password = loginForm.elements.password.value;
+
+//     if (email.trim() === '' || password.trim() === '') {
+//       alert('Будь ласка, заповніть всі поля');
+//     } else {
+//       const formData = {
+//         email: email,
+//         password: password
+//       };
+
+//       console.log(formData);
+//       loginForm.reset(); 
+//     }
+//   });
+// });
+/* //////////////////////////    8    /////////////////////////////// */ 
+
+
+/* //////////////////////////    9    /////////////////////////////// */
+// function getRandomHexColor() {
+//     return `#${Math.floor(Math.random() * 16777215)
+//       .toString(16)
+//       .padStart(6, '0')}`;
+//   }
+  
+//   document.addEventListener('DOMContentLoaded', function() {
+//     const widget = document.querySelector('.widget');
+//     const colorSpan = widget.querySelector('.color');
+//     const changeColorButton = widget.querySelector('.change-color');
+  
+//     changeColorButton.addEventListener('click', function() {
+//       const newColor = getRandomHexColor();
+//       document.body.style.backgroundColor = newColor;
+//       colorSpan.textContent = newColor;
+//     });
+//   });
+  
+/* //////////////////////////    9    /////////////////////////////// */ 
+
+
+/* //////////////////////////    10    /////////////////////////////// */ 
+// function getRandomHexColor() {
+//     return `#${Math.floor(Math.random() * 16777215)
+//       .toString(16)
+//       .padStart(6, '0')}`;
+//   }
+  
+//   document.addEventListener('DOMContentLoaded', function() {
+//     const controls = document.querySelector('#controls');
+//     const boxesContainer = document.querySelector('#boxes');
+  
+//     const createButton = controls.querySelector('[data-create]');
+//     const destroyButton = controls.querySelector('[data-destroy]');
+//     const input = controls.querySelector('input');
+  
+//     createButton.addEventListener('click', createBoxes);
+//     destroyButton.addEventListener('click', destroyBoxes);
+  
+//     function createBoxes() {
+//       const amount = Number(input.value);
+  
+//       if (isNaN(amount) || amount < 1 || amount > 100) {
+//         alert('Please enter a valid number between 1 and 100.');
+//         return;
+//       }
+  
+//       const fragment = document.createDocumentFragment();
+  
+//       for (let i = 0; i < amount; i++) {
+//         const size = 30 + i * 10;
+//         const color = getRandomHexColor();
+  
+//         const box = document.createElement('div');
+//         box.style.width = `${size}px`;
+//         box.style.height = `${size}px`;
+//         box.style.backgroundColor = color;
+  
+//         fragment.appendChild(box);
+//       }
+  
+//       boxesContainer.appendChild(fragment);
+//     }
+  
+//     function destroyBoxes() {
+//       boxesContainer.innerHTML = '';
+//     }
+//   });
+  
+/* //////////////////////////    10   /////////////////////////////// */ 
+
+
 
 
 
